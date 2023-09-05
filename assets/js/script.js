4809a04a69dd30572cafd5195deec8e4d4423fa9
AntoninoCucuzza <cucuzza.antonino@hotmail.com>
2023-09-05 18:42:28 +0200

    refactoring new way to get date

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,19 @@ Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione
 Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina!
 
 */
-function oraAdesso() {
-    let today = new Date();
-    let date = today.getFullYear()+'-'+(today.getMonth() + 1)+'-'+today.getDate();
-    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    let dateTime = date+' '+time;
-     
-    return dateTime
-    
-}
 
-const clock = setInterval(timer, 1000);
 
-function timer() {
-    console.log(oraAdesso());
-}
+let newLesson = new Date("September 6, 2023 9:30:00").getTime();
+
+// Scrivo l'intervallo
+const clock = setInterval(function () {
+  let now = new Date().getTime();
+  let lessonTime = newLesson - now;
+  console.log(lessonTime);
+  if (lessonTime == 0) {
+    clearInterval(clock);
+    alert("Inizia la lezione!");
+  } else {
+    lessonTime--;
+  }
+}, 1000);
