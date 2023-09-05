7368a9955506e9ce32b9e03109641b90bba650fb
AntoninoCucuzza <cucuzza.antonino@hotmail.com>
2023-09-05 19:29:45 +0200

    get h and min

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,15 +9,19 @@ Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina
 
 let newLesson = new Date("September 6, 2023 9:30:00").getTime();
 
-// Scrivo l'intervallo
+
 const clock = setInterval(function () {
   let now = new Date().getTime();
   let lessonTime = newLesson - now;
   console.log(lessonTime);
+
+  let hour = (lessonTime / (1000 * 60 * 60))
+  let min = (lessonTime /(1000 * 60 ))
+  console.log(min.toFixed(0));
+  console.log(hour.toFixed(0));
+
   if (lessonTime == 0) {
     clearInterval(clock);
     alert("Inizia la lezione!");
-  } else {
-    lessonTime--;
-  }
+  } 
 }, 1000);
