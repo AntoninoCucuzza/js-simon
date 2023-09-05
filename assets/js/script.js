c289d233e8ff7a0d7e30b17f65127ffca273f8b5
AntoninoCucuzza <cucuzza.antonino@hotmail.com>
2023-09-05 15:13:19 +0200

    add counter

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,4 +5,18 @@ Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione
 Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina!
 
 */
+function oraAdesso() {
+    let today = new Date();
+    let date = today.getFullYear()+'-'+(today.getMonth() + 1)+'-'+today.getDate();
+    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    let dateTime = date+' '+time;
+     
+    return dateTime
+    
+}
 
+const clock = setInterval(timer, 1000);
+
+function timer() {
+    console.log(oraAdesso());
+}
